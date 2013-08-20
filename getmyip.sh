fe501f06ef34b49bbb5a2e82efc59ad2ae0b07c4
Sam Betts <sam@code-smash.net>
2013-08-20 17:49:25 +0100

    Made IP image a hidden file

--- getmyip.sh
+++ getmyip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 a="`curl checkip.dyndns.org | sed -e 's/.*: //' -e 's/<.*$//'`"
-/usr/local/bin/convert -size 420x95 xc:transparent -font /Library/Fonts/Arial.ttf -gravity Center -fill "#e0e0e0" -pointsize 56 -draw "text 0,0 '$a'" -rotate -90 ~/image.png
+/usr/local/bin/convert -size 420x95 xc:transparent -font /Library/Fonts/Arial.ttf -gravity Center -fill "#e0e0e0" -pointsize 56 -draw "text 0,0 '$a'" -rotate -90 ~/.image.png
 echo `date '+%A %x'`
